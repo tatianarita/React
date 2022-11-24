1899c7d2181a34dc7dffe3fea84403d24a9e058a
tatianarita <tatianarita.28@gmail.com>
2022-11-23 21:43:54 -0300

    modificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 
 import ComponentContainer from './components/ComponentContainer/ComponentContainer'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,26 @@
+import CartWidget from "../CartWidget/CartWidget"
 
 const NavBar = () => {
   return (
     <div>
-        <li><a href="Inicio">Inicio</a></li>
-        <li><a href="Tienda">Tienda</a></li>
-        <li><a href="Contacto">Contacto</a></li>
-        <li><a href="Nosotros">Nosotros </a></li>    
+        <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Tienda</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-link active" aria-current="page" href="#">Plantas</a>
+        <a className="nav-link" href="#">Macetas</a>
+        <a className="nav-link" href="#">Jardines Verticales</a>
+        <a className="nav-link disabled"><CartWidget></CartWidget><div>4</div></a>
+        
+      </div>
     </div>
+  </div>
+</nav>
+</div>
   )
 }
 
